ae2cf97e508d838c4c401044b9c53ec8faa79911
Ludvik Haltuf <ludvik.haltuf@gmail.com>
2019-07-04 22:49:02 +0200

    A - Return current date in JSON

---
+++ bidirect-a/src/main/java/com/cerlite/playground/bidirecta/CurrentDateDto.java
@@ -0,0 +1,23 @@
+package com.cerlite.playground.bidirecta;
+
+public class CurrentDateDto {
+
+    private String message;
+    private String currentDate;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getCurrentDate() {
+        return currentDate;
+    }
+
+    public void setCurrentDate(String currentDate) {
+        this.currentDate = currentDate;
+    }
+}
--- bidirect-a/src/main/java/com/cerlite/playground/bidirecta/HelloController.java
+++ bidirect-a/src/main/java/com/cerlite/playground/bidirecta/HelloController.java
@@ -1,14 +1,23 @@
 package com.cerlite.playground.bidirecta;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 @RestController
 public class HelloController {
 
-    @GetMapping
-    public String getIndex() {
-        return "hello";
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public CurrentDateDto getIndex() {
+        CurrentDateDto currentDateDto = new CurrentDateDto();
+        currentDateDto.setMessage("Current Date");
+        LocalDate date = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        currentDateDto.setCurrentDate(date.format(formatter));
+        return currentDateDto;
     }
 
 }
