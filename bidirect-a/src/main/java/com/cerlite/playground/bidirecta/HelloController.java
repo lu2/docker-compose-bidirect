022565a229e231171f34046385c34b8c7c0d488d
Ludvik Haltuf <ludvik.haltuf@gmail.com>
2019-08-06 15:20:31 +0200

    Add request logging

--- bidirect-a/src/main/java/com/cerlite/playground/bidirecta/HelloController.java
+++ bidirect-a/src/main/java/com/cerlite/playground/bidirecta/HelloController.java
@@ -1,6 +1,8 @@
 package com.cerlite.playground.bidirecta;
 
 import com.cerlite.playground.bidirect.common.dto.CurrentDateDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +13,11 @@ import java.time.format.DateTimeFormatter;
 @RestController
 public class HelloController {
 
+    private static final Logger LOG = LoggerFactory.getLogger(HelloController.class);
+
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public CurrentDateDto getIndex() {
+        LOG.info("GetCurrentDate requested");
         CurrentDateDto currentDateDto = new CurrentDateDto();
         currentDateDto.setMessage("Current Date");
         LocalDate date = LocalDate.now();
